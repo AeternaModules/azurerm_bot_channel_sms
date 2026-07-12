188878c924819f83683f7b6a6b59fdef5262b31e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,26 @@
-output "bot_channel_smses" {
-  description = "All bot_channel_sms resources"
-  value       = azurerm_bot_channel_sms.bot_channel_smses
-  sensitive   = true
-}
 output "bot_channel_smses_bot_name" {
-  description = "List of bot_name values across all bot_channel_smses"
-  value       = [for k, v in azurerm_bot_channel_sms.bot_channel_smses : v.bot_name]
+  description = "Map of bot_name values across all bot_channel_smses, keyed the same as var.bot_channel_smses"
+  value       = { for k, v in azurerm_bot_channel_sms.bot_channel_smses : k => v.bot_name }
 }
 output "bot_channel_smses_location" {
-  description = "List of location values across all bot_channel_smses"
-  value       = [for k, v in azurerm_bot_channel_sms.bot_channel_smses : v.location]
+  description = "Map of location values across all bot_channel_smses, keyed the same as var.bot_channel_smses"
+  value       = { for k, v in azurerm_bot_channel_sms.bot_channel_smses : k => v.location }
 }
 output "bot_channel_smses_phone_number" {
-  description = "List of phone_number values across all bot_channel_smses"
-  value       = [for k, v in azurerm_bot_channel_sms.bot_channel_smses : v.phone_number]
+  description = "Map of phone_number values across all bot_channel_smses, keyed the same as var.bot_channel_smses"
+  value       = { for k, v in azurerm_bot_channel_sms.bot_channel_smses : k => v.phone_number }
 }
 output "bot_channel_smses_resource_group_name" {
-  description = "List of resource_group_name values across all bot_channel_smses"
-  value       = [for k, v in azurerm_bot_channel_sms.bot_channel_smses : v.resource_group_name]
+  description = "Map of resource_group_name values across all bot_channel_smses, keyed the same as var.bot_channel_smses"
+  value       = { for k, v in azurerm_bot_channel_sms.bot_channel_smses : k => v.resource_group_name }
 }
 output "bot_channel_smses_sms_channel_account_security_id" {
-  description = "List of sms_channel_account_security_id values across all bot_channel_smses"
-  value       = [for k, v in azurerm_bot_channel_sms.bot_channel_smses : v.sms_channel_account_security_id]
+  description = "Map of sms_channel_account_security_id values across all bot_channel_smses, keyed the same as var.bot_channel_smses"
+  value       = { for k, v in azurerm_bot_channel_sms.bot_channel_smses : k => v.sms_channel_account_security_id }
 }
 output "bot_channel_smses_sms_channel_auth_token" {
-  description = "List of sms_channel_auth_token values across all bot_channel_smses"
-  value       = [for k, v in azurerm_bot_channel_sms.bot_channel_smses : v.sms_channel_auth_token]
+  description = "Map of sms_channel_auth_token values across all bot_channel_smses, keyed the same as var.bot_channel_smses"
+  value       = { for k, v in azurerm_bot_channel_sms.bot_channel_smses : k => v.sms_channel_auth_token }
   sensitive   = true
 }
 
